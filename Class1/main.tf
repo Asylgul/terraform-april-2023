c1c3c53d8190eb356f168ddb9ad85630b63736d2
asya89kg@gmail.com <asya89kg@gmail.com>
2023-07-02 19:31:14 +0000

    class1

--- Class1/main.tf
+++ Class1/main.tf
@@ -1,3 +1,20 @@
 resource "aws_iam_user" "user" {
   name = "kaizen"
   }
+
+ resource "aws_iam_group" "group" {
+  name = "engineers"
+} 
+
+
+resource "aws_iam_user" "user1" {
+  name = "kaizen1"
+  }
+
+  resource "aws_iam_user" "user2" {
+  name = "kaizen2"
+  }
+
+  resource "aws_iam_user" "group" {
+  name = "engineers"
+  }
